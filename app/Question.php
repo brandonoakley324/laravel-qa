fa726cf2d29cd5a8990c425fe249d58f0f171d67
Brandon Oakley <brandonoakley324@gmail.com>
2019-02-20 23:43:14 -0400

    Prepopulate users and questions table using model factories and seeder

--- app/Question.php
+++ app/Question.php
@@ -8,10 +8,16 @@ class Question extends Model
 {
 
     protected $fillable = [ 'title' , 'body'];
-    
+
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+
+    public function setTitleAttribute($value) {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
     }
     
 }
---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+use App\Question;
+
+$factory->define(App\Question::class, function (Faker $faker) {
+    return [
+        'title' => rtrim($faker->sentence(rand(5,10)), '.'),
+        'body' => $faker->paragraphs(rand(3,7), true),  //true converts it to string rather than array
+        'views' => rand(0, 10),
+        'answers' => rand(0, 10),
+        'votes' => rand(-3, 10),        //I didnt set votes as an unsigned int in the table migration
+        
+    ];
+});
